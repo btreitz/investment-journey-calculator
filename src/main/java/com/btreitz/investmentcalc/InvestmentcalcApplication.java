0fad481cc1b2c75f0377a56df1e861fc901bde43
Bastian Treitz <basian.treitz@stud.htwk-leipzig.de>
2020-11-18 21:43:21 +0100

    Initialize Project

---
+++ src/main/java/com/btreitz/investmentcalc/InvestmentcalcApplication.java
@@ -0,0 +1,13 @@
+package com.btreitz.investmentcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentcalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentcalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/btreitz/investmentcalc/InvestmentcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btreitz.investmentcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentcalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
