702600028d917d6f28e2679b35fa457ed709fd13
Bastian Treitz <basian.treitz@stud.htwk-leipzig.de>
2020-11-18 23:36:21 +0100

    [ADD] calculation mapping

--- src/main/java/com/btreitz/investmentcalc/CalculationController.java
+++ src/main/java/com/btreitz/investmentcalc/CalculationController.java
@@ -3,13 +3,30 @@ package com.btreitz.investmentcalc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CalculationController {
 
+    private final ICalculationService calculationService;
+
+    public CalculationController(ICalculationService calculationService) {
+        this.calculationService = calculationService;
+    }
+
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/calculate")
+    public CalculationResult calculateResults(
+            @RequestParam(name = "initialInvestment") double initialInvestment,
+            @RequestParam(name = "periodicContribution") double periodicContribution,
+            @RequestParam(name = "contributionFrequency") int contributionFrequency,
+            @RequestParam(name = "annualGrowth") double annualGrowth,
+            @RequestParam(name = "duration") int duration) {
+        return calculationService.calculateResults(initialInvestment, periodicContribution, contributionFrequency, annualGrowth, duration);
     }
 
 }
