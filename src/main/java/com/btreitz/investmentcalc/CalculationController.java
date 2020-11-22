ae93651b9c875674701c623df88ed5e5fc87fad3
Bastian Treitz <bastian.treitz@stud.htwk-leipzig.de>
2020-11-22 23:27:09 +0100

    [ADD] ERROR Message for users

--- src/main/java/com/btreitz/investmentcalc/CalculationController.java
+++ src/main/java/com/btreitz/investmentcalc/CalculationController.java
@@ -10,22 +10,43 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import java.util.ArrayList;
 
 @Controller
 @Validated
 public class CalculationController {
 
     private final ICalculationService calculationService;
+    private Query mostRecentQuery;
 
     public CalculationController(ICalculationService calculationService) {
         this.calculationService = calculationService;
+        this.mostRecentQuery = new Query(0, 0, 1, 0, 1);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
-        return new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+    String handleConstraintViolationException(Model model, ConstraintViolationException e) {
+        new ResponseEntity<>("not valid due to validation error: " + e.getMessage(), HttpStatus.BAD_REQUEST);
+        ArrayList<String> errorMessages = new ArrayList<>();
+        if (e.getMessage().contains("initialInvestment")) {
+            errorMessages.add("Initial Investment must be of value 0 or above");
+        }
+        if (e.getMessage().contains("periodicContribution")) {
+            errorMessages.add("Periodic Contribution must be of value 0 or above");
+        }
+        if (e.getMessage().contains("contributionFrequency")) {
+            errorMessages.add("No valid Contribution Frequency was selected");
+        }
+        if (e.getMessage().contains("annualGrowth")) {
+            errorMessages.add("Annual Growth must be in percentage between the values -100.0 and 100.0");
+        }
+        if (e.getMessage().contains("duration")) {
+            errorMessages.add("Duration must be of value 0 and below 100");
+        }
+        model.addAttribute("errors", errorMessages);
+        model.addAttribute("query", this.mostRecentQuery);
+        return "index";
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
@@ -42,7 +63,8 @@ public class CalculationController {
             @RequestParam(name = "annualGrowth") @Min(-100) @Max(100) double annualGrowth,
             @RequestParam(name = "duration") @Min(1) @Max(100) int duration) {
         CalculationResult calculationResult = calculationService.calculateResults(initialInvestment, periodicContribution, contributionFrequency, annualGrowth, duration);
-        model.addAttribute("query", new Query(initialInvestment, periodicContribution, contributionFrequency, annualGrowth, duration));
+        this.mostRecentQuery = new Query(initialInvestment, periodicContribution, contributionFrequency, annualGrowth, duration);
+        model.addAttribute("query", this.mostRecentQuery);
         model.addAttribute("calculationResult", calculationResult);
         return "index";
     }
